d9f232807cb4edd264d551ac647c35b39f6e8c14
medsonmoombe <medsonmoombe21@gmail.com>
2022-06-15 14:42:33 +0200

    Modify the app.js file

--- src/index.js
+++ src/index.js
@@ -10,15 +10,14 @@ import store from './redux/configureStore';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
-  <React.StrictMode>
-    <Provider store={store}>
+  <Provider store={store}>
+    <React.StrictMode>
       <Router>
         <App />
       </Router>
-    </Provider>
-  </React.StrictMode>
-  ,
-
+    </React.StrictMode>
+    ,
+  </Provider>,
 );
 
 // If you want to start measuring performance in your app, pass a function
