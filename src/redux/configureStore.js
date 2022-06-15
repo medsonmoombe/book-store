25299dea491c946ba0f487dbc7f0c8b14e2e250c
medsonmoombe <medsonmoombe21@gmail.com>
2022-06-15 14:37:33 +0200

    Removed unused code

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,6 +1,5 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-// import logger from './logger';
 import bookReducer from './books/books';
 import checkStatusReducer from './categories/categories';
 
