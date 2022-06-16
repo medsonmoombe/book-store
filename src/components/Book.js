3cd1ba13614b470750869e585ceae4ac12e68cf9
medsonmoombe <medsonmoombe21@gmail.com>
2022-06-16 18:18:23 +0200

    Add the styels and modify css files

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Books from './Books';
-import './Book.css';
 
 const BookContent = ({ books }) => (
   <ul className="book-content">
