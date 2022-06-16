56f60e2686db57935ca2d7bde849f6efd64421f0
medsonmoombe <medsonmoombe21@gmail.com>
2022-06-16 17:41:32 +0200

    Add the styles to display in the desktop version

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Books from './Books';
+import './Book.css';
 
 const BookContent = ({ books }) => (
   <ul className="book-content">
